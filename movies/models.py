27846e18e358a8da6e6ab79b2e5ea707c496c52e
David Simek <david.simek1998@gmail.com>
2022-03-27 11:52:11 +0200

    added movie model

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Movie(models.Model):
+    LANGUAGE_ENG = 'eng'
+    LANGUAGE_CZ = 'cz'
+
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENG, 'english'),
+        (LANGUAGE_CZ, 'czech'),
+    )
+
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+    rating = models.IntegerField(validators=[
+        MinValueValidator(0), MaxValueValidator(100)
+    ])
+    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=5)
