f655be9a3255498ba9d1febaef62cd487a3c83ce
David Simek <david.simek1998@gmail.com>
2022-04-02 13:21:19 +0200

    added template inheritance

--- movies/urls.py
+++ movies/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from movies.views import homepage
+from movies.views import homepage_view, actors_view
 
 urlpatterns = [
-    path('', homepage),
+    path('', homepage_view, name='homepage'),
+    path('actors/', actors_view, name='all_actors')
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,8 @@
 from django.template.response import TemplateResponse
-from movies.models import Movie
+from movies.models import Movie, Actor
 
 
-def homepage(request):
+def homepage_view(request):
     all_movies = Movie.objects.all().order_by('-rating')  # SELECT * FROM movies_movie;
     best_movies = Movie.objects.filter(rating__gte=80).order_by('-rating')  # SELECT * FROM movies_movie WHERE rating GTE 80;
     worst_movies = Movie.objects.filter(rating__lte=20).order_by('rating')
@@ -10,5 +10,15 @@ def homepage(request):
         'all_movies': all_movies,
         'best_movies': best_movies,
         'worst_movies': worst_movies,
+        'page_name': 'Homepage',
     }
     return TemplateResponse(request, 'homepage.html', context=context)
+
+
+def actors_view(request):
+    actors = Actor.objects.all()
+    context = {
+        'all_actors': actors,
+        'page_name': 'Actors',
+    }
+    return TemplateResponse(request, 'actors.html', context=context)
