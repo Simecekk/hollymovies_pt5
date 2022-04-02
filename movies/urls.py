0384067f6900b6642907035596c89b08593a9290
David Simek <david.simek1998@gmail.com>
2022-04-02 14:39:44 +0200

    added movies view

--- movies/urls.py
+++ movies/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from movies.views import homepage_view, actors_view
+from movies.views import homepage_view, actors_view, movies_view
 
 urlpatterns = [
     path('', homepage_view, name='homepage'),
-    path('actors/', actors_view, name='all_actors')
+    path('actors/', actors_view, name='actors'),
+    path('movies/', movies_view, name='movies')
 ]
