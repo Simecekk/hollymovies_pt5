c052b47b9ef5acde563739196142912388a42a07
David Simek <david.simek1998@gmail.com>
2022-04-02 12:00:00 +0200

    added actor admin

--- movies/admin.py
+++ movies/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 
-from movies.models import Movie
+from movies.models import Movie, Actor
 
 
 class MovieAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'language', 'rating')
+    list_filter = ('language', )
+
+
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'age', 'gender')
+    list_filter = ('gender', )
 
 
 admin.site.register(Movie, MovieAdmin)
+admin.site.register(Actor, ActorAdmin)
