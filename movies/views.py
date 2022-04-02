f33f98b0c8683ddf9eeb9a2a6656bf5df8a5268c
David Simek <david.simek1998@gmail.com>
2022-04-02 10:38:06 +0200

    added order_by + count to homepage

--- movies/views.py
+++ movies/views.py
@@ -3,9 +3,9 @@ from movies.models import Movie
 
 
 def homepage(request):
-    all_movies = Movie.objects.all()
-    best_movies = Movie.objects.filter(rating__gte=80)
-    worst_movies = Movie.objects.filter(rating__lte=20)
+    all_movies = Movie.objects.all().order_by('-rating')  # SELECT * FROM movies_movie;
+    best_movies = Movie.objects.filter(rating__gte=80).order_by('-rating')  # SELECT * FROM movies_movie WHERE rating GTE 80;
+    worst_movies = Movie.objects.filter(rating__lte=20).order_by('rating')
     context = {
         'all_movies': all_movies,
         'best_movies': best_movies,
