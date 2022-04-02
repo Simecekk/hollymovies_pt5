6acfe17495f41556be84e89ec095ef552e977223
David Simek <david.simek1998@gmail.com>
2022-04-02 13:47:15 +0200

    updated homepage

--- movies/views.py
+++ movies/views.py
@@ -3,14 +3,9 @@ from movies.models import Movie, Actor
 
 
 def homepage_view(request):
-    all_movies = Movie.objects.all().order_by('-rating')  # SELECT * FROM movies_movie;
-    best_movies = Movie.objects.filter(rating__gte=80).order_by('-rating')  # SELECT * FROM movies_movie WHERE rating GTE 80;
-    worst_movies = Movie.objects.filter(rating__lte=20).order_by('rating')
     context = {
-        'all_movies': all_movies,
-        'best_movies': best_movies,
-        'worst_movies': worst_movies,
-        'page_name': 'Homepage',
+        'number_of_movies': Movie.objects.all().count(),
+        'number_of_actors': Actor.objects.all().count(),
     }
     return TemplateResponse(request, 'homepage.html', context=context)
 
@@ -22,3 +17,31 @@ def actors_view(request):
         'page_name': 'Actors',
     }
     return TemplateResponse(request, 'actors.html', context=context)
+
+
+# all_movies = Movie.objects.all().order_by('-rating')  # SELECT * FROM movies_movie;
+# best_movies = Movie.objects.filter(rating__gte=80).order_by('-rating')  # SELECT * FROM movies_movie WHERE rating GTE 80;
+# worst_movies = Movie.objects.filter(rating__lte=20).order_by('rating')
+# context = {
+#     'all_movies': all_movies,
+#     'best_movies': best_movies,
+#     'worst_movies': worst_movies,
+#     'page_name': 'Homepage',
+# }
+
+
+ #    <h4>All movies ({{ all_movies.count }})</h4>
+ #    {% for movie in all_movies %}
+ #        <p>{{ movie.name }} - rating ({{ movie.rating }})</p>
+ #    {% endfor %}<br>
+ #
+ #
+ #    <h4>Best movies ({{ best_movies.count }})</h4>
+ #    {% for movie in best_movies %}
+ #        <p>{{ movie.name }} - rating ({{ movie.rating }})</p>
+ #    {% endfor %}<br>
+ #
+ #    <h4>Worst movies ({{ worst_movies.count }})</h4>
+ #    {% for movie in worst_movies %}
+ #        <p>{{ movie.name }} - rating ({{ movie.rating }})</p>
+ #    {% endfor %}
